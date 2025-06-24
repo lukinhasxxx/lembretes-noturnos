044b05cec40c3c7fcba39c44e3e851934d150b13
lucas.edi@outlook.com <lucas.edi@outlook.com>
2025-06-24 17:17:41 -0300

    chore: add funcionalidade de salvar estado nos cards

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,14 @@
 import './Colaborador.css'
 
-const Colaborador = (props) => {
+const Colaborador = ({nome,imagem,cargo}) => {
     return (
         <div className='colaborador'>
             <div className='cabecalho'>
-                <img src = 'https://github.com/lukinhasxxx.png' alt = 'nome alternativo'/>
+                <img src = {imagem} alt = {nome}/>
             </div>
             <div className='rodape'>
-                <h4>{props.nome}</h4>
-                <h5> o brabo</h5>
+                <h4>{nome}</h4>
+                <h5> {cargo}</h5>
             </div>
         </div>
     )
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -9,7 +9,13 @@ const Time = (props) => {
         <section className='time' style={{ backgroundColor: props.corSecundaria}}>
 
             <h3 style={primaryColor} >{props.nome}</h3>
-            <Colaborador/>
+
+            <div className='colaboradores'>
+                {props.colaboradores.map( colaborador => <Colaborador nome = {colaborador.nome} cargo = {colaborador.cargo} imagem = {colaborador.imagem} />)}
+            </div>
+
+            
+            
         </section>
 
     )
