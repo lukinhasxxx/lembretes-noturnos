6ca48a77fe123b718332911dee88f9ed1654ad1f
lucas.edi@outlook.com <lucas.edi@outlook.com>
2025-06-27 16:16:20 -0300

    style: finaliza rodape desktop-only

--- src/componentes/Footer/index.js
+++ src/componentes/Footer/index.js
@@ -3,16 +3,26 @@ import './footer.css'
 const Footer = () => {
     return (
             <footer>
-                <section>
-                    <a href = 'www.facebook.com' >
-                        <img src='/imagens/fb.png' alt = 'logo do facebook'/>
-                    </a>
-                    <a href = 'www.x.com'> 
-                         <img src='/imagens/tw.png' alt = 'logo do facebook'/>
-                    </a>
-                    <a href = 'www.instagram.com'> 
-                        <img src='/imagens/ig.png' alt = 'logo do facebook'/>
-                    </a>
+                <section className='itens-rodape'>
+                    <div className='rede-social'>
+                        <a href = 'www.facebook.com' >
+                            <img src='/imagens/fb.png' alt = 'logo do facebook'/>
+                        </a>
+                        <a href = 'www.x.com'> 
+                            <img src='/imagens/tw.png' alt = 'logo do facebook'/>
+                        </a>
+                        <a href = 'www.instagram.com'> 
+                            <img src='/imagens/ig.png' alt = 'logo do facebook'/>
+                        </a>
+                    </div>
+                    <div>
+                            <img src='/imagens/logo-organo.png' alt = "logo-nome-organo"/>
+                    </div>
+                    <div className='texto-rodape'>
+                        <h3>Desenvolvido por Lucas.</h3>
+                    </div>
+
+
                 </section>
             </footer>
     )
