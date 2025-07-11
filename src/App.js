6ee2f04ed38b6feab305aeff7187756003866876
lucas.edi@outlook.com <lucas.edi@outlook.com>
2025-07-11 17:08:10 -0300

    chrore: adiciona UUID's e passa via PROPS para o componente Time

--- src/App.js
+++ src/App.js
@@ -1,48 +1,47 @@
-
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import { useState } from 'react';
 import Time from './componentes/Time'
 import Footer from './componentes/Footer';
-
+import { v4 as uuidv4 } from 'uuid';
 
 
 function App() {
 
   const [times, setTimes] = useState([
     {  
+      id:uuidv4(),
       nome:'Programacao',
-      
       cor:'#D9F7E9'
     },
     {  
+      id:uuidv4(),
       nome:'Front-End',
-      
       cor:'#E8F8FF'
     },
     {  
+      id:uuidv4(),
       nome:'Data Science',
-      
       cor:'#F0F8E2'
     },
     {  
+      id:uuidv4(),
       nome:'Devops',
-      
       cor:'#FDE7E8'
     },
     {  
+      id:uuidv4(),
       nome:'UX e Design',
-      
       cor:'#FAE9F5'
     },
     {  
+      id:uuidv4(),
       nome:'Mobile',
-      
       cor:'#FFF5D9'
     },
     {  
+      id:uuidv4(),
       nome:'Inovacao e Gestao',
-      
       cor:'#FFEEDF'
     }
   ])
@@ -57,11 +56,12 @@ function App() {
     console.log("deletando colaborador")
     }
 
-  function mudarCorDoTime (cor, nome) {
+  function mudarCorDoTime (cor, id) {
     setTimes(times.map( time =>
     {
-      if(time.nome === nome) {
-          return { ...time, cor: cor }
+      if(time.id === id) {
+          time.cor=cor;
+          console.log(id)
       } 
       return time;}
       
@@ -76,6 +76,7 @@ function App() {
 
       {times.map(time => 
         <Time  
+          id={time.id} 
           mudarCor = {mudarCorDoTime}
           key = {time.nome}
           nome={time.nome}
