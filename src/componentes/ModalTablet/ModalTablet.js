3d71ed4967478fca7f9e36a32e742be69442ae67
lucas.edi@outlook.com <lucas.edi@outlook.com>
2025-09-05 04:13:02 -0300

    feat: adiciona horario real e ajusta barra de tarefas

--- src/componentes/ModalTablet/ModalTablet.js
+++ src/componentes/ModalTablet/ModalTablet.js
@@ -1,6 +1,7 @@
 import './ModalTablet.css'
 import Botao from '../Botao'
 import { useState } from 'react'
+import Relogio from '../Relogio/Relogio'
 
 const ModalTablet = ({aoSubmeter, validarLigadoDesligado}) => {
     const [nome, setNome] = useState('')
@@ -15,13 +16,31 @@ const ModalTablet = ({aoSubmeter, validarLigadoDesligado}) => {
     <div>
             <div className='tablet-tela'  > 
             <div className='area-de-trabalho' style={{backgroundImage:"url('/imagens/windows/windowsWallpaper.jpg')",backgroundRepeat:"no-repeat"}}  >
+
                 {telaAtiva === 'desktop' && (<div className='icone-lembretes'
                    onClick={() => setTelaAtiva('lembretes') }>
                     <img src='/imagens/windows/iconeLembretes.png'
                     alt='abrir lembretes' />
                     <span>Lembretes.exe</span>
                 </div>)}
+
+                {telaAtiva === 'desktop' && (<div className='icone-led'
+                   onClick={() => setTelaAtiva('icone-led') }>
+                    <img src='/imagens/windows/ledIcone.png'
+                    alt='abrir lembretes' />
+                    <span>Config.exe</span>
+                </div>)}
+
+                {telaAtiva ==='icone-led' && (<div className='janela-configuracao-led' >
+
+                        <span>Aqui vai ficar a configuracao teste</span>
+                    </div>
+                )}
+
                 { telaAtiva === 'lembretes' && (
+
+                <div className='janela-lembretes' >
+                    <img src="/imagens/windows/iconeFechar.png" alt="Icone de fechar" />
                     <form onSubmit={aoSalvar}>
                     <h2>Deixe seu lembrete para ser incluído no painel.</h2>
                     <textarea
@@ -35,20 +54,47 @@ const ModalTablet = ({aoSubmeter, validarLigadoDesligado}) => {
                 
                     <Botao>Enviar lembrete</Botao>
                 </form>
+            </div>
                 )}
             </div>
 
     <div className="barra-de-tarefas">
-      {
+    
+        <div className='menu-iniciar' >
+            <img src="/imagens/windows/menuIniciar.png" alt="Menu iniciar" />
+        </div>
+
         <div className="seta-voltar" onClick={() => setTelaAtiva('desktop')}>
           <img src="/imagens/windows/setaVoltar.png" alt="Voltar para o Desktop" />
         </div>
-      }
+        
+       <div className='icone-bateria' >
+            <img src="/imagens/windows/bateriaIcone.png" alt="Bateria carregando" />
+        </div> 
     
+       <div className='icone-wifi' >
+            <img src="/imagens/windows/wifiIcone.png" alt="\Icone wifi" />
+        </div> 
+
+        <div className='icone-som' >
+            <img src="/imagens/windows/semSomIcone.png" alt="Icone sem som" />
+        </div> 
+
+        <div className='idioma'>
+            <span>POR</span>
+            <br></br>
+            <span>PTB2</span>
+        </div>
+
+        <Relogio/>
+
+        <div className='icone-notificacoes' >
+        <img src="/imagens/windows/iconeNotificacoes.png" alt="Icone de notificacoes" />
+        </div> 
+
     </div>
 
         </div>
-
             <section className='secao-tablet-modal'>
                 <img className='tablet-modal' src='/imagens/tabletModal.png'
                 style={{display: validarLigadoDesligado ? "" : " none" }}
