fc0f54ba79a0163ae1dd1329ff872446a7d7af0d
lucas.edi@outlook.com <lucas.edi@outlook.com>
2025-09-10 16:51:18 -0300

    feat(cards):adiciona funcao de excluir e marcar cards

--- src/componentes/Colaborador/CardDeNotas.js
+++ src/componentes/Colaborador/CardDeNotas.js
@@ -1,13 +1,49 @@
+import { useState } from 'react'
 import './CardDeNotas.css'
 
 
-const CardDeNotas = ({lembretes}) => {
+const CardDeNotas = ({lembretes, aoFixar, aoDeletar}) => {
 
+const [fixarLembrete, setFixarLembrete] = useState(false)
 
-    return (
+const validarFixado = () => {
+    setFixarLembrete(fixarLembrete => !fixarLembrete)
+    aoFixar(lembretes.id)
+    console.log('foi clicado e o statos do favoritar eh',lembretes.id)
+}
+
+const deletarItem = () => {
+aoDeletar(lembretes.id)
+}
+
+    return ( //lembra que a borda baixo ta FIXA ela NAO ta mexendo com a rolagem, PROAVELMENTE pelo after e before, importante
         <div className='cards'>
             <div className="card" >
+            <img className='lixeira'
+                 src='/imagens/windows/lixeira.svg'
+                  alt='lixeira'
+                  onClick={() => deletarItem()}
+                   />
+
+            {
+                fixarLembrete === false && (
+                <img className='pinOff'
+                 src='/imagens/windows/pinOff.png'
+                  alt='pin fixar'
+                  onClick={() => validarFixado()}
+                   />
+                )
+            }
                 <h4>{lembretes.texto}</h4>
+            {
+                fixarLembrete === true && (
+                <img className='pinOn'
+                 src='/imagens/windows/pinOn.png'
+                 alt='pin desfixar'
+                 onClick={() => validarFixado()}
+                   />
+                )
+            }
             </div>
         </div>
         )
